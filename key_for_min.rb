2948e4c5fb5bbd5103b302b6402733e7039a7e46
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 05:56:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   if name_hash.length == 0
     return NIL
-  else
-    name_hash.collect do |key, value|
+#  else
+#    name_hash.collect do |key, value|
   end    
 end
