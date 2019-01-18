2656fcfd40a041c69e062954f7200bdbd99f5764
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 05:58:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,6 @@ def key_for_min_value(name_hash)
   lowest_key == NIL
   lowest_value == NIL
   name_hash.each do |key, value|
-    if value < lowest_value
+    if value == nil || value < lowest_value
   end    
 end
