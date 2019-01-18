3824eb8d843f7ee0e36afc7f9651153ba48434cc
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 05:57:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.length == 0
+  lowest_key == NIL
     return NIL
 #  else
 #    name_hash.collect do |key, value|
